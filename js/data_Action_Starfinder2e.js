379d8ddaae8a743e495f9828f4613b967a0ba1a9
Niuzio <antoniopadron511@gmail.com>
2025-08-27 16:36:12 -0400

    Update data_Action_Starfinder2e.js

--- js/data_Action_Starfinder2e.js
+++ js/data_Action_Starfinder2e.js
@@ -3,7 +3,7 @@ data_Action_Starfinder2e = [
     title: "Puntos de Héroe",
     icon: "star",
     subtitle: "Recursos heroicos",
-    description: "Tus logros heroicos te garantizan buena fortuna. Puedes tener un máximo de 3 puntos de héroe.",
+    description: "Tus logros heroicos te garantizan buena fortuna. Puedes tener un máximo de 3 puntos de héroe.[one-action] [two-actions] [three-actions] [reaction]",
     reference: "Pantalla del Director – Puntos de Héroe",
     tags       : [
       { name: "Goblin", info: "Herencia goblin de las nieves" },
@@ -11,7 +11,7 @@ data_Action_Starfinder2e = [
     ],
     bullets: [
       "Ganas 1 punto de héroe al inicio de la sesión.",
-      "Ganas 1 punto de héroe por hora de juego o al realizar un acto heroico."[one-action] [two-actions] [three-actions] [reaction],
+      "Ganas 1 punto de héroe por hora de juego o al realizar un acto heroico.",
       "Gastas 1 punto de héroe para repetir una tirada y usar el segundo resultado (efecto de Fortuna).",
       "Gastas 3 puntos de héroe para eliminar la condición de moribundo y estabilizarte con 0 PS (Recuperación Heroica).",
       "Pierdes todos tus puntos de héroe al final de la sesión."
