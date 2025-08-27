9cb77a78e941bce1c362fef4feb845cf2c73050c
Niuzio <antoniopadron511@gmail.com>
2025-08-27 19:12:22 -0400

    Update quickref.js

--- js/quickref.js
+++ js/quickref.js
@@ -10,50 +10,52 @@ function replaceMarkers(str) {
         .replaceAll('[free-action]',   '<img src="icons/free_action.png"   class="action-icon" alt="acción libre">');
 }
 function add_quickref_item(parent, data, type) {
-  // Si el objeto trae “header”, creamos una nueva subcategoría
-  if ( data.header ) {
-    // Creamos el wrapper .category
-    var category = document.createElement("div");
-    category.className = "category";
-    parent.appendChild(category);
+    // 1) Si viene un header, creamos una nueva subcategoría
+    if (data.header) {
+        // wrapper .category
+        var category = document.createElement("div");
+        category.className = "category";
+        parent.appendChild(category);
 
-    // Creamos el título de la subcategoría
-    var h = document.createElement("div");
-    h.className = "section-subheader";
-    h.textContent = data.header;
-    category.appendChild(h);
+        // título de la subcategoría
+        var h = document.createElement("div");
+        h.className = "section-subheader";
+        h.textContent = data.header;
+        category.appendChild(h);
 
-    // Preparamos el contenedor de ítems (grid)
-    lastItemsContainer = document.createElement("div");
-    lastItemsContainer.className = "item-group";
-    category.appendChild(lastItemsContainer);
+        // grid para los ítems siguientes
+        lastItemsContainer = document.createElement("div");
+        lastItemsContainer.className = "item-group";
+        category.appendChild(lastItemsContainer);
 
-    return;
-  }
-    var icon = data.icon || "perspective-dice-six-faces-one";
+        // salimos para que este objeto no genere un ítem normal
+        return;
+    }
+
+    // 2) Ítem normal: icono, título, descripción
+    var icon     = data.icon     || "perspective-dice-six-faces-one";
     var subtitle = replaceMarkers(data.subtitle || "");
     var title    = replaceMarkers(data.title    || "[no title]");
 
     var item = document.createElement("div");
-    item.className += "item itemsize"
-    item.innerHTML =
-    '\
-    <div class="item-icon iconsize icon-' + icon + '"></div>\
-    <div class="item-text-container text">\
-        <div class="item-title">' + title + '</div>\
-        <div class="item-desc">' + subtitle + '</div>\
-    </div>\
-    ';
+    item.className = "item itemsize";
+    item.innerHTML = `
+        <div class="item-icon iconsize icon-${icon}"></div>
+        <div class="item-text-container text">
+            <div class="item-title">${title}</div>
+            <div class="item-desc">${subtitle}</div>
+        </div>
+    `;
 
     var style = window.getComputedStyle(parent.parentNode.parentNode);
     var color = style.backgroundColor;
-
-    item.onclick = function () {
+    item.onclick = function() {
         show_modal(data, color, type);
-    }
-  // en vez de parent.appendChild(item);
-  var container = lastItemsContainer || parent;
-  container.appendChild(item);
+    };
+
+    // 3) Decidimos dónde insertar: en la última subcategoría activa o en parent
+    var container = lastItemsContainer || parent;
+    container.appendChild(item);
 }
 
 function show_modal(data, color, type) {
