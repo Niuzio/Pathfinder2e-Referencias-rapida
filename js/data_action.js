ac4fb0a25aa5b68aec9546161816a874cf7cc269
Niuzio <antoniopadron511@gmail.com>
2025-08-27 02:30:39 -0400

    Update data_action.js

--- js/data_action.js
+++ js/data_action.js
@@ -3,6 +3,10 @@ data_action = [
     title: "Activar un objeto",
     icon: "variable_action",
     subtitle: "Manipular",
+    tags       : [
+      { name: "Goblin", info: "Herencia goblin de las nieves" },
+      { name: "Ascendencia", info: "Guía de personajes de los Presagios Perdidos" }
+    ],
     description: "Llamas al efecto de un objeto activándolo de la forma adecuada.",
     reference: "PF2e SRD p.532",
     traits: ["manipular"],
