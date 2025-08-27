1b330c24a0711bcf4e419fc101f47bf60680b2db
Niuzio <antoniopadron511@gmail.com>
2025-08-27 16:35:20 -0400

    Update data_Action_Starfinder2e.js

--- js/data_Action_Starfinder2e.js
+++ js/data_Action_Starfinder2e.js
@@ -11,7 +11,7 @@ data_Action_Starfinder2e = [
     ],
     bullets: [
       "Ganas 1 punto de héroe al inicio de la sesión.",
-      "Ganas 1 punto de héroe por hora de juego o al realizar un acto heroico.",
+      "Ganas 1 punto de héroe por hora de juego o al realizar un acto heroico."[one-action] [two-actions] [three-actions] [reaction],
       "Gastas 1 punto de héroe para repetir una tirada y usar el segundo resultado (efecto de Fortuna).",
       "Gastas 3 puntos de héroe para eliminar la condición de moribundo y estabilizarte con 0 PS (Recuperación Heroica).",
       "Pierdes todos tus puntos de héroe al final de la sesión."
