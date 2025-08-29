616ce6a30cbd0e51597f48c97bfc53f65a72f591
Niuzio <antoniopadron511@gmail.com>
2025-08-28 21:03:17 -0400

    Update data_action.js

--- js/data_action.js
+++ js/data_action.js
@@ -1,8 +1,8 @@
-data_action = [
+ data_action = [
   {
     "title": "Activar un objeto",
     "icon": "snatch",
-    "subtitle": "Llamas al efecto de un objeto activándolo de la forma adecuada.",
+    "subtitle": " Activar un objeto",
     "description": "<p><strong>Requisitos</strong> Sólo puedes Activar un objeto con el rasgo investido si eres tú quien lo inviste. Si el objeto requiere que Interactúes con él, debes estarlo empuñando (si es un objeto empuñable) o tocándolo con una mano libre (si es otro tipo de objeto).</p><p><br></p><p>&nbsp;Llamas al efecto de un objeto activándolo de la forma adecuada. Ésta es una actividad especial que requiere un número variable de acciones, que se indican en el bloque de estadísticas del objeto. Algunos objetos se pueden activar como una reacción o como una acción gratuita. En este caso, Activas el objeto como reacción o como acción gratuita (según el caso) en lugar de como actividad. Algunos casos se indican en la entrada de Activar del objeto, en su bloque de estadísticas: por ejemplo, ‘<strong>Activar</strong> [reaction] ordenar’. </p><p><br></p><p><strong>Tiempos de activación largos</strong>  Algunos objetos tardan minutos u horas en activarse. La actividad Activar un objeto para dichos objetos incluye una mezcla de los componentes de activación indicados, pero no es necesario detallar cuál llevas a cabo en un momento dado. No puedes usar otras acciones o reacciones mientras activas uno de estos objetos, aunque a discreción del DJ, podrías pronunciar unas cuantas frases. Como con las demás actividades que requieren bastante tiempo, dichas activaciones tienen el rasgo exploración y no puedes llevarlas a cabo en un encuentro. Si estalla el combate mientras estás activando una, la activación queda perturbada (consulta la barra lateral Per turbar activaciones de la página anterior). </p><p><br></p><p><strong>Componentes de activación</strong> Cada entrada de activación indica cualquier componente implicado en la activación después 532 de los iconos de acción o del texto, como por ejemplo ‘[one-action] ordenar’. Los componentes de activación, que se describen más abajo, añaden rasgos (indicados entre paréntesis) y Requisitos a la activación. Si no puedes proporcionar los componentes, no consigues Activar el objeto. </p><ul><li> <strong>Ordenar </strong>(auditivo, concentrar) </li><li> <strong>Visualizar </strong>(concentrar) </li><li> <strong>Interactuar </strong>(manipular)</li><li> <strong>Lanzar un conjuro</strong></li></ul>",
     "reference": "PF2e SRD p.532",
     "traits": [
@@ -17,7 +17,7 @@ data_action = [
   {
     "title": "Prestar ayuda [reaction]",
     "icon": "telepathy",
-    "subtitle": " Chequeo DC 15 para dar un  bonicador +1 de circunstancia a  un chequeo de habilidad asistido o  a una tirada de ataque (éxito  crítico: +2, +3 si es maestro, +4 si  es legendario).",
+    "subtitle": " Tratas de ayudar a un aliado en una tarea.",
     "description": "<p><strong>Desencadenante</strong> Un aliado está a punto de utilizar una acción que requiere una prueba de habilidad o una tirada de ataque.</p><p><br></p><p><strong>Requisitos</strong> El aliado está dispuesto a aceptar tu ayuda y te has pre parado para prestarla (ver a continuación) </p><p><br></p><p>Tratas de ayudar a un aliado en una tarea. Para utilizar esta reacción, primero tienes que prepararte para ayudar, por lo general utilizando una acción durante tu turno. Tienes que explicar al DJ exactamente cómo lo haces, y él determina si puedes Prestar ayuda a tu aliado. </p><p><br></p><p>Cuando utilizas tu reacción Prestar ayuda, haz una prueba de habilidad o tirada de ataque de un tipo decidido por el DJ. La CD típica es 15, pero el DJ podría ajustarla para tareas particularmente difíciles o fáciles. El DJ puede añadir cualquier rasgo relevante a tu acción preparatoria o a tu acción de Prestar ayuda dependiendo de la situa ción, o incluso permitirte hacer pruebas de Prestar ayuda diferentes a las pruebas de habilidad y a las tiradas de ataque. </p><p><br></p><ul><li><strong>Éxito crítico</strong> Concedes a tu aliado un bonificador +2 por circunstancia a la prueba desencadenante. Si tienes el rango maestro en la prueba que haces, el bonificador es +3 y si tienes el rango legen dario, es +4. </li><li><strong>Éxito</strong> Concedes a tu aliado un bonificador +1 por circunstancia a la prueba desencadenante. </li><li><strong>Fallo crítico</strong> Tu aliado sufre un penalizador -1 por circunstancia a la prueba desencadenante.</li></ul>",
     "reference": "Manual de juego p.417",
     "traits": [
@@ -28,7 +28,7 @@ data_action = [
   {
     "title": "Retrasar [free-action]",
     "icon": "stopwatch",
-    "subtitle": " Selecciona esta opción cuando  comience su turno; tomar el turno mas tarde",
+    "subtitle": " tomar el turno mas tarde",
     "description": "<p>&nbsp;<strong>Desencadenante </strong>Empieza tu turno.</p><p><br></p><p>Esperas al momento preciso para actuar. El resto de tu turno aún no&nbsp;tiene lugar. En su lugar, se te retira del orden de iniciativa. Puedes volver al mismo como acción gratuita desencadenada por el final del turno de cualquier otra criatura. Esto cambia permanentemente tu iniciativa a la nueva posición. No puedes usar reacciones hasta volver al orden de iniciativa. Si Retrasas un asalto entero sin volver al orden de iniciativa, las acciones del turno Retrasado se pierden, tu iniciativa no cambia y tu siguiente turno tiene lugar en tu posición original en el orden de iniciativa. </p><p>Cuando Retrasas, cualquier daño persistente u otros efectos negativos que normalmente tienen lugar al inicio o al final de tu turno suceden de inmediato cuando utilizas la acción Retrasar. Cualquier efecto beneficioso que acabaría en cualquier momento de tu turno también acaba. El DJ podría determinar que otros efectos también se acaban cuando Retrasas. Esencialmente, no puedes Retrasar para evitar consecuencias negativas que tendrían lugar en tu turno ni para prolongar efectos beneficiosos que acabarían en tu turno.</p>",
     "reference": "Manual de juego p.418",
     "traits": [],
@@ -48,7 +48,7 @@ data_action = [
   {
     "title": "Interactuar [one-action]",
     "icon": "snatch",
-    "subtitle": " Coge un objeto, abre una  puerta, dibuja un elemento o  realiza una acción similar.",
+    "subtitle": "Manipular un objeto o el terreno",
     "description": "<p>Utilizas tu mano o tus manos para manipular un objeto o el terreno. Puedes hacerte con un objeto desatendido o almacenado, desenvainar un arma, intercambiar por otro un objeto sostenido (pág. 268), abrir una puerta o conseguir un efecto similar. En raras ocasiones, podrías tener que hacer una prueba de habilidad para determinar si tu acción de Interactuar tiene éxito</p>",
     "reference": "Manual de juego p.417",
     "traits": [
@@ -59,7 +59,7 @@ data_action = [
   {
     "title": "Montar [one-action]",
     "icon": "run",
-    "subtitle": "Subirte a una criatura, mayor que tú, para montarla o desmontarla.",
+    "subtitle": "Subirte a una criatura o desmontarla",
     "description": "<p>&nbsp;<strong>Requisitos </strong>Estás adyacente a una criatura por lo menos un tamaño mayor que tú, que está dispuesta a dejarse montar. </p><p><br></p><p>Te mueves hasta la criatura y la montas. Si ya estás montado, en su lugar puedes utilizar esta acción para desmontar, abandonando la montura en un espacio adyacente a la misma.</p>",
     "reference": "Manual de juego p.417",
     "traits": [
@@ -70,7 +70,7 @@ data_action = [
   {
     "title": "Preparar [two-actions]",
     "icon": "two_actions",
-    "subtitle": "Prepárate para  realizar una única acción o una  acción gratuita como reacción con  un desencadenante que designes.",
+    "subtitle": "Prepárate un desencadenante que designes.",
     "description": "<p>Te preparas para utilizar una acción que tendrá lugar fuera de tu tur no. Elige una acción individual o acción gratuita que puedes utilizar y designa un desencadenante. A continuación se acaba tu turno. Si el desencadenante designado se cumple antes del inicio de tu siguiente turno, puedes utilizar la acción elegida como una reacción (si aún cumples los requisitos para utilizarla). No puedes Preparar una acción gratuita que ya tiene desencadenante. </p><p>Si tienes un penalizador por ataque múltiple y tu acción preparada es una acción de ataque, tu ataque preparado sufrirá el penalizador por ataque múltiple que correspondiera en el momento en el que utilizaste Preparar. Esta es una de las pocas veces en las que se aplica el penalizador por ataque múltiple cuando no es tu turno.</p>",
     "reference": "Manual de juego p.417",
     "traits": [
@@ -81,7 +81,7 @@ data_action = [
   {
     "title": "Soltar [free-action]",
     "icon": "snatch",
-    "subtitle": " Suelta algo que estés sujetando  sin provocar reacciones.",
+    "subtitle": "Suelta algo que estés sujetando",
     "description": "<p>Sueltas algo que sostienes en una o ambas manos. Esto podría significar que dejas caer un objeto, quitas una mano de tu arma pero continúas empuñándola con la otra, dejas ir una cuerda de la que cuelga un candelabro o llevas a cabo una acción similar. A diferencia de la mayoría de acciones de manipular, Soltar no desencadena reacciones que pueden verse activadas por acciones con el rasgo manipular (por ejemplo, Golpe reactivo). </p><p>Si quieres prepararte para Soltar algo fuera de tu turno, usa la actividad Preparar.</p>",
     "reference": "Manual de juego p.418",
     "traits": [
@@ -92,7 +92,7 @@ data_action = [
   {
     "title": "Mantener [one-action]",
     "icon": "embrassed-energy",
-    "subtitle": "Extender el efecto de un conjuro/objeto hasta el final prox. turno.",
+    "subtitle": "Extender el efecto de un conjuro/objeto",
     "description": "<p>Elige uno de tus efectos con una duración mantenida o que indica un beneficio especial cuando lo Mantienes. La mayoría de dichos efectos proceden de conjuros o de activaciones de objetos mágicos. Si el efecto tiene una duración mantenida, se prolonga hasta el final de tu siguiente turno (mantener más de una vez en el mismo tur no no prolonga la duración a turnos subsiguientes). Si una aptitud&nbsp;puede ser mantenida pero no indica cuánto tiempo, puede serlo hasta 10 minutos. </p><p><br></p><p>Un efecto podría indicar un beneficio adicional que tiene lugar cuando lo Mantienes, y esto puede incluso aparecer en efectos que no tienen una duración mantenida. Si el efecto tiene a la vez un beneficio especial y una duración mantenida, tu acción de Mantener prolonga la duración así como el hecho de disponer del beneficio especial.</p><p> </p><p>Si tu acción de Mantener es perturbada, la aptitud se acaba.</p>",
     "reference": "Manual de juego p.419",
     "traits": [
