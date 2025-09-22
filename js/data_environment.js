38e8579b1171e791d9408a6dac595d24ea99ffaf
Niuzio <antoniopadron511@gmail.com>
2025-09-22 02:41:35 -0400

    Update data_environment.js

--- js/data_environment.js
+++ js/data_environment.js
@@ -29,31 +29,6 @@ data_environment_obscurance = [
 ]
 
 data_environment_light = [
-    {
-    title: "Visión en la oscuridad",
-    icon: "semi-closed-eye",
-    subtitle: "Darkvision",
-    description: "Permite ver en la oscuridad como si fuera luz tenue.",
-    reference: "Reglas básicas – Sentidos especiales",
-    bullets: [
-      "Ignora los efectos de la oscuridad para ver.",
-      "Solo distingue tonos de gris, no colores."
-    ]
-  },
-  {
-    title: "Visión verdadera",
-    icon: "eye-shield",
-    subtitle: "True Seeing",
-    description: "Revela ilusiones, criaturas invisibles y presencias en el Plano Etéreo.",
-    reference: "Reglas básicas – Sentidos especiales",
-    bullets: [
-      "Percibe criaturas e ilusiones de forma auténtica.",
-      "No es afectada por invisibilidad ni ilusiones."
-        ]
-    }
-]
-
-data_environment_vision = [
   {
     "title": " Visión en la oscuridad  y visión en la oscuridad mayor",
     "icon": "semi-closed-eye",
@@ -75,7 +50,7 @@ data_environment_vision = [
   {
     "title": "Observado",
     "icon": "beast-eye",
-    "subtitle": "detección",
+    "subtitle": "",
     "reference": "Manual de jugador p.434",
     "description": "<p>&nbsp;En la mayoría de circunstancias, puedes sentir a las criaturas sin dificultad y designarlas como objetivo de la forma normal. Las criaturas en ese estado están observadas. Observar requiere un sentido preciso, que para la mayoría de las criaturas significa la vista. Si no puedes observar a la criatura, está escondida, no de tectada o inadvertida y tienes que tener en cuenta las restricciones que ello comporta para designarla como objetivo. Incluso si una criatura está observada, también puede estar oculta.</p>",
     "bullets": [],
@@ -84,25 +59,25 @@ data_environment_vision = [
   {
     "title": " Escondido",
     "icon": "hidden",
-    "subtitle": "detección",
+    "subtitle": "",
     "reference": "Manual de jugador p.434",
-    "description": "<p>&nbsp;Una criatura escondida es mínimamente perceptible. Sabes qué es pacio ocupa, pero poco más. Quizá la criatura acaba de ponerse a cubierto y ha utilizado con éxito la acción Esconderse. Tu objetivo puede estar tras una catarata, donde puedes ver algo de movimien to pero no puedes determinar una ubicación exacta. Quizá has sido cegado o la criatura es invisible, pero utilizaste la acción básica Buscar para determinar su ubicación general basándote sólo en el oído. Sean cuales sean los puntos específicos, estás desprevenido ante una criatura escondida.</p><p><br></p><p>Cuando designas como objetivo a una criatura escondida, an tes de tirar para determinar tu efecto tienes que hacer una prueba plana CD 11. Si fallas, no afectas a la criatura, aunque las accio nes utilizadas se siguen gastando, así como cualquier espacio de conjuro, coste y otros recursos. Estás desprevenido ante la criatu ra, tanto si has conseguido designarla como objetivo como si no.</p>",
+    "description": "<p>&nbsp;Una criatura escondida es mínimamente perceptible. Sabes qué es pacio ocupa, pero poco más. Quizá la criatura acaba de ponerse a cubierto y ha utilizado con éxito la acción Esconderse. Tu objetivo puede estar tras una catarata, donde puedes ver algo de movimien to pero no puedes determinar una ubicación exacta. Quizá has sido cegado o la criatura es invisible, pero utilizaste la acción básica Buscar para determinar su ubicación general basándote sólo en el oído. Sean cuales sean los puntos específicos, estás desprevenido ante una criatura escondida. Cuando designas como objetivo a una criatura escondida, an tes de tirar para determinar tu efecto tienes que hacer una prueba plana CD 11. Si fallas, no afectas a la criatura, aunque las accio nes utilizadas se siguen gastando, así como cualquier espacio de conjuro, coste y otros recursos. Estás desprevenido ante la criatu ra, tanto si has conseguido designarla como objetivo como si no.</p>",
     "bullets": [],
     "tags": []
   },
   {
     "title": " No detectado",
     "icon": "double-face-mask",
-    "subtitle": "detección",
+    "subtitle": "",
     "reference": "Manual de jugador p.434",
-    "description": "<p>&nbsp;Si la criatura no ha sido detectada, no sabes qué espacio ocupa, estás desprevenido ante ella y no puedes designarla como objetivo con facilidad. Utilizar la acción básica Buscar puede ayudarte a en contrar una criatura no detectada, por lo general haciéndola pasar a escondida por lo que a ti respecta. Si la criatura no está detecta da, eso no quiere decir que no seas consciente de su presencia; po drías sospechar que hay una criatura no detectada en la habitación contigo, incluso sin ser capaz de determinar el espacio que ocupa. 434 El estado no detectado cubre las criaturas cuya presencia ignoras. Designar como objetivo a una criatura no detectada es difícil. Si sospechas que hay una criatura, puedes elegir una casilla y hacer un ataque. Esto funciona igual que designar como objetivo a una cria tura escondida, pero la prueba plana y la tirada de ataque las hace ambas en secreto el DJ. Éste no te dirá por qué has fallado, ya sea por fallar la prueba plana, por obtener un resultado insuficiente en la tirada de ataque o por elegir una casilla equivocada. El DJ podría permitirte designar como objetivo a una criatura no detectada con algún conjuro u otra aptitud de una forma similar. Las criaturas no detectadas están sujetas a los efectos de área de la forma normal. </p><p>Por ejemplo, supongamos que un mago elfo enemigo lanza invisibilidad y después utiliza Movimiento furtivo para alejarse. Sospechas que con la Velocidad de 30 pies (9 m) que tiene, proba blemente ha movido 15 pies (4,5 m) hacia una puerta abierta. Te mueves y atacas un espacio a 15 pies (4,5 m) de donde el elfo inició su movimiento y directamente en el camino hacia la puerta. El DJ hace en secreto una tirada de ataque y una prueba plana, sabiendo que tu suposición no era correcta del todo: ¡en realidad el elfo esta ba en un espacio adyacente! A continuación te informa de que has fallado, por lo que decides hacer tu siguiente ataque en el espacio adyacente, por si acaso. Esta vez, es el espacio correcto y tanto la tirada secreta de ataque como la prueba plana del DJ tienen éxito, ¡por lo que aciertas!</p>",
+    "description": "<p>&nbsp;Si la criatura no ha sido detectada, no sabes qué espacio ocupa, estás desprevenido ante ella y no puedes designarla como objetivo con facilidad. Utilizar la acción básica Buscar puede ayudarte a en contrar una criatura no detectada, por lo general haciéndola pasar a escondida por lo que a ti respecta. Si la criatura no está detecta da, eso no quiere decir que no seas consciente de su presencia; po drías sospechar que hay una criatura no detectada en la habitación contigo, incluso sin ser capaz de determinar el espacio que ocupa. 434 El estado no detectado cubre las criaturas cuya presencia ignoras. Designar como objetivo a una criatura no detectada es difícil. Si sospechas que hay una criatura, puedes elegir una casilla y hacer un ataque. Esto funciona igual que designar como objetivo a una cria tura escondida, pero la prueba plana y la tirada de ataque las hace ambas en secreto el DJ. Éste no te dirá por qué has fallado, ya sea por fallar la prueba plana, por obtener un resultado insuficiente en la tirada de ataque o por elegir una casilla equivocada. El DJ podría permitirte designar como objetivo a una criatura no detectada con algún conjuro u otra aptitud de una forma similar. Las criaturas no detectadas están sujetas a los efectos de área de la forma normal. Por ejemplo, supongamos que un mago elfo enemigo lanza invisibilidad y después utiliza Movimiento furtivo para alejarse. Sospechas que con la Velocidad de 30 pies (9 m) que tiene, proba blemente ha movido 15 pies (4,5 m) hacia una puerta abierta. Te mueves y atacas un espacio a 15 pies (4,5 m) de donde el elfo inició su movimiento y directamente en el camino hacia la puerta. El DJ hace en secreto una tirada de ataque y una prueba plana, sabiendo que tu suposición no era correcta del todo: ¡en realidad el elfo esta ba en un espacio adyacente! A continuación te informa de que has fallado, por lo que decides hacer tu siguiente ataque en el espacio adyacente, por si acaso. Esta vez, es el espacio correcto y tanto la tirada secreta de ataque como la prueba plana del DJ tienen éxito, ¡por lo que aciertas!</p>",
     "bullets": [],
     "tags": []
   },
   {
     "title": " Inadvertido ",
     "icon": "hidden",
-    "subtitle": "detección",
+    "subtitle": "",
     "reference": "Manual de jugador p.434",
     "description": "<p>Si no tienes ni idea de que hay una criatura presente, dicha cria tura pasa inadvertida para ti. Una criatura no detectada tam bién podría estar inadvertida. Este estado suele importar para las aptitudes que pueden usarse sólo contra objetivos que no son totalmente conscientes de tu presencia.</p>",
     "bullets": [],
@@ -111,7 +86,7 @@ data_environment_vision = [
   {
     "title": "Oculto",
     "icon": "hidden",
-    "subtitle": "detección",
+    "subtitle": "",
     "reference": "Manual de jugador p.434",
     "description": "<p>&nbsp;Una criatura oculta está en la niebla, en luz tenue o entre alguna otra cosa que oscurece la vista pero que no es una barrera física. Cuando designas como objetivo a una criatura oculta de ti, tienes que hacer una prueba plana CD 5 antes de tirar para determinar tu efecto. Si fallas, no afectas al objetivo. El estado oculto no cambia cuál de las categorías principales de detección se le aplica. Una criatura en un banco de niebla poco denso sigue estando observa da aunque está oculta.</p>",
     "bullets": [],
@@ -120,9 +95,48 @@ data_environment_vision = [
   {
     "title": " Invisible",
     "icon": "invisible",
-    "subtitle": "detección",
+    "subtitle": "",
     "reference": "Manual de jugador p.434",
-    "description": "<p>&nbsp;Una criatura con el estado invisible (mediante un conjuro de in visibilidad, por ejemplo) está automáticamente no detectada ante cualquier criatura que se basa en la vista cómo su único sentido preciso. Los sentidos precisos distintos a la vista ignoran el estado invisible. Puedes Buscar (pág. 416) para imaginarte donde está una criatura invisible y hacer que tan solo esté escondida de ti. Esto dura hasta que la criatura invisible usa con éxito Movimiento fur tivo para quedar de nuevo no detectada. Si estás observando a una criatura cuando se hace invisible, empieza escondida puesto que sabes dónde estaba cuando se hizo invisible, aunque a continua ción puede usar Movimiento furtivo para quedar no detectada.</p><p> Otros efectos pueden frustrar parcialmente la invisibilidad. Por ejemplo, si estás rastreando las pisadas de una criatura invisible so bre la nieve, las propias pisadas la convierten en simplemente escon dida. Lanzar una red sobre una criatura invisible haría que pasara a estar observada (pero oculta) mientras tuviera la red encima.</p>",
+    "description": "<p>&nbsp;Una criatura con el estado invisible (mediante un conjuro de in visibilidad, por ejemplo) está automáticamente no detectada ante cualquier criatura que se basa en la vista cómo su único sentido preciso. Los sentidos precisos distintos a la vista ignoran el estado invisible. Puedes Buscar (pág. 416) para imaginarte donde está una criatura invisible y hacer que tan solo esté escondida de ti. Esto dura hasta que la criatura invisible usa con éxito Movimiento fur tivo para quedar de nuevo no detectada. Si estás observando a una criatura cuando se hace invisible, empieza escondida puesto que sabes dónde estaba cuando se hizo invisible, aunque a continua ción puede usar Movimiento furtivo para quedar no detectada. Otros efectos pueden frustrar parcialmente la invisibilidad. Por ejemplo, si estás rastreando las pisadas de una criatura invisible so bre la nieve, las propias pisadas la convierten en simplemente escon dida. Lanzar una red sobre una criatura invisible haría que pasara a estar observada (pero oculta) mientras tuviera la red encima.</p>",
+    "bullets": [],
+    "tags": []
+  }
+]
+
+data_environment_vision = [
+  {
+    "title": "Olfato",
+    "icon": "nose",
+    "subtitle": "Sentidos",
+    "description": "<p>&nbsp;El olfato implica detectar criaturas u objetos mediante el olfato, y suele ser un sentido vago. El rango se indica en la aptitud y sólo funciona si la criatura u objeto a detectar emite un aroma. Si la criatura emite un fuerte aroma o está a barlovento, el DJ puede doblar o incluso triplicar el rango de las aptitudes de olfato utili zadas para detectar dicha criatura, o puede reducirlo si la criatura esta a sotavento.</p>",
+    "reference": "Manual de jugador p.433",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Sentidos precisos",
+    "icon": "one-eyed",
+    "subtitle": "Sentidos",
+    "description": "<p>&nbsp;La vista normal es un sentido preciso, que se puede utilizar para percibir el mundo con un detalle matizado. La única forma para designar como objetivo a una criatura sin tener inconvenientes es utilizar un sentido preciso. Por lo general puedes detectar a una criatura automáticamente con un sentido preciso si no se está es condiendo o está oscurecida por el entorno, en cuyo caso puedes utilizar la acción básica Buscar para detectarla mejor.</p>",
+    "reference": "Manual de jugador p.433",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": " Sentidos imprecisos",
+    "icon": "one-eyed",
+    "subtitle": "Sentidos",
+    "reference": "Manual de jugador p.433",
+    "description": "<p>&nbsp;El oído normal es un sentido impreciso, porque no puede detectar la gama completa de detalles de un sentido preciso. Por lo general puedes sentir automáticamente a una criatura con un sentido impreciso, pero tiene el estado escondido en lugar de observado. Podría estar no detectado por ti si utiliza Movimiento furtivo o está en un entorno que distorsiona el sentido, como una habitación ruidosa en el caso del oído. En dichos casos, tienes que utilizar la acción básica Buscar para detectar a la criatura. En el mejor de los casos, un sentido impreciso puede utilizarse para conseguir que una criatura no detectada (o una que ni siquiera sabías que estaba allí) esté simple mente escondida; no puede hacer que la criatura quede observada.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": " Sentidos vagos",
+    "icon": "one-eyed",
+    "subtitle": "Sentidos",
+    "reference": "Manual de jugador p.433",
+    "description": "<p>&nbsp;Un PJ también dispone de muchos sentidos vagos, que le pueden alertar de que hay algo allí pero no son útiles para centrarse en ello y determinar exactamente de qué se trata. El más útil de ellos para un PJ típico es el sentido del olfato. En el mejor de los casos, un sentido vago se puede utilizar para detectar la presencia de una criatura no detectada, haciendo que pase a detectada.</p><p> Incluso entonces, el sentido vago no es suficiente para conseguir que la criatura pase a escondida u observada. Cuando una criatura podría detectar a otra, el DJ casi siempre utiliza el sentido más preciso disponible. </p><p>Las reglas de Pathfinder suponen que una criatura determina da tiene la vista como su único sentido preciso y el oído como su único sentido impreciso. Sin embargo, algunos PJs y criaturas disponen de sentidos precisos o imprecisos que no encajan con esta suposición. Por ejemplo, un PJ con mala vista podría tratar dicho sentido como impreciso, un animal con la aptitud olfato puede utilizar su sentido del olfato como sentido impreciso y una criatura con ecolocalización o una aptitud similar puede utilizar el oído como sentido preciso. Dichos sentidos reciben a menudo nombres especiales y aparecen como “ecolocalización (preciso)”, “olfato (impreciso) 30 pies (9 m)”, etc.</p>",
     "bullets": [],
     "tags": []
   }
