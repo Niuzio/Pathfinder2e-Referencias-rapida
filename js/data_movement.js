cff331f41d47a9af05afa52e844c496665ffe809
Niuzio <antoniopadron511@gmail.com>
2025-09-24 15:59:40 -0400

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -1,6 +1,6 @@
 data_movement = [
   {
-    "title": "Zancada (Stride) [one-action]",
+    "title": "Zancada [one-action]",
     "icon": "run",
     "subtitle": " Moverte hasta tu  Velocidad.",
     "description": "Te mueves hasta tu Velocidad (pág. 420).",
@@ -12,7 +12,7 @@ data_movement = [
     ]
   },
   {
-    "title": "Golpear (Strike) [one-action]",
+    "title": "Golpear [one-action]",
     "icon": "crossed-swords",
     "subtitle": " Ataque con un arma o  ataque desarmado.",
     "description": "<p>Atacas con un arma que empuñas o con un ataque sin armas, de signando como objetivo a una criatura a tu alcance (para un ataque cuerpo a cuerpo) o dentro del rango de distancia (para un ataque a distancia). Haz una tirada de ataque utilizando el modificador al ataque para el arma o ataque sin armas que estás utilizando, y compara el resultado con la CA de la criatura objetivo para determinar el efecto. </p><p><br></p><ul><li><strong>Éxito crítico</strong> Haz una tirada de daño según el arma o el ataque sin armas, infligiendo doble daño (ver pág. 407 para las reglas sobre cómo se dobla el daño). </li><li><strong>Éxito </strong>Haz una tirada de daño según el arma o el ataque sin armas, infligiendo daño normal</li></ul>",
@@ -27,7 +27,7 @@ data_movement = [
     ]
   },
   {
-    "title": "Lanzar un conjuro (Cast a Spell) [two-actions] (Típicamente 2 acciones)",
+    "title": "Lanzar un conjuro [two-actions] (Típicamente 2 acciones)",
     "icon": "magic-swirl",
     "subtitle": "Lanzar un hechizo que has preparado o de tu repertorio.",
     "description": "<p>&nbsp;Los conjuros pueden variar en la cantidad de acciones que requieren, tal y como se muestra en el bloque de estadísticas de cada conjuro. Lanzas trucos, conjuros a partir de espacios de conjuro y conjuros de foco utilizando idéntico proceso, pero tienes que gastar el conjuro cuando lo lanzas desde un espacio de conjuro y tienes que gastar 1 Punto de Foco para lanzar un conjuro de foco. Algunas reglas hacen referencia a la actividad Lanzar un conjuro, como por ejemplo \"si la siguiente acción que utilizas es Lanzar un conjuro\". Cualquier conjuro califica como una actividad de Lanzar un conjuro y cualquier característica del mismo utiliza las del conjuro específico que estás lanzando</p>",
@@ -40,7 +40,7 @@ data_movement = [
     ]
   },
   {
-    "title": "Paso (Step) [one-action]",
+    "title": "Paso [one-action]",
     "icon": "journey",
     "subtitle": "Moverse 5 pies sin desencadenar reacciones.",
     "description": "<p>&nbsp;<strong>Requisitos </strong>Tu Velocidad es por lo menos 10 pies (3 m). </p><p><br></p><p>Te mueves cuidadosamente 5 pies (1,5 m). A diferencia de la mayoría de tipos de movimiento, dar un Paso no desencadena reacciones (del tipo Golpe reactivo) que pueden ser desencadenadas por acciones de movimiento o por entrar o salir de una casilla. No puedes dar un Paso en terreno difícil (pág. 423) ni darlo utilizando una Velocidad diferente a tu Velocidad terrestre</p>",
@@ -50,7 +50,7 @@ data_movement = [
     "bullets": []
   },
   {
-    "title": "Alzar un escudo (Raise a Shield) [one-action] ",
+    "title": "Alzar un escudo [one-action] ",
     "icon": "round-shield",
     "subtitle": "Alzas un escudo para obtener su modificador a la CA.",
     "description": "<p>&nbsp;<strong>Requisitos </strong>Estás embrazando un escudo. </p><p><br></p><p>Colocas el escudo para que te proteja. Cuando has Alzado un escudo, obtienes el bonificador por circunstancia indicado a la CA. Tu escudo permanece alzado hasta el inicio de tu siguiente turno.</p>",
