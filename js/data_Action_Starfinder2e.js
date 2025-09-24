97ffe1bf374725f28c3e3269b5a1c32e373aaa08
Niuzio <antoniopadron511@gmail.com>
2025-09-23 22:13:06 -0400

    Update data_Action_Starfinder2e.js

--- js/data_Action_Starfinder2e.js
+++ js/data_Action_Starfinder2e.js
@@ -1,20 +1,11 @@
 data_Action_Starfinder2e = [
   {
-    title: "Puntos de Héroe[one-action]",
+    title: "Proximamente",
     icon: "star",
-    subtitle: "Recursos heroicos[one-action]",
-    description: "Tus logros heroicos te garantizan buena fortuna. Puedes tener un máximo de 3 puntos de héroe.[one-action] [two-actions] [three-actions] [reaction]",
-    reference: "Pantalla del Director – Puntos de Héroe",
-    tags       : [
-      { name: "Goblin", info: "Herencia goblin de las nieves" },
-      { name: "Ascendencia", info: "Guía de personajes de los Presagios Perdidos" }
-    ],
-    bullets: [
-      "Ganas 1 punto de héroe al inicio de la sesión.",
-      "Ganas 1 punto de héroe por hora de juego o al realizar un acto heroico.",
-      "Gastas 1 punto de héroe para [one-action] repetir una tirada y usar el segundo resultado (efecto de Fortuna).",
-      "Gastas 3 puntos de héroe para eliminar la condición de moribundo y estabilizarte con 0 PS (Recuperación Heroica).",
-      "Pierdes todos tus puntos de héroe al final de la sesión."
-    ]
+    subtitle: "Proximamente",
+    description: "Proximamente",
+    reference: "Proximamente",
+    tags       : [ ],
+    bullets: [ ]
   }
 ];
