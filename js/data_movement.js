ff0bcd7a73ff61205e45798adc260917a495cd05
Niuzio <antoniopadron511@gmail.com>
2025-09-25 21:11:59 -0400

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -3,10 +3,14 @@ data_movement = [
     "title": "Zancada [one-action]",
     "icon": "run",
     "subtitle": " Moverte hasta tu  Velocidad.",
-    "description": "Te mueves hasta tu Velocidad (pág. 420).",
+    "description": "<p>Te mueves hasta tu Velocidad (pág. 420).</p>",
     "reference": "Manual de juego p.421",
-    "tags": [ { name: "Movimiento", info: "(rasgo) Una acción con este rasgo implica moverse de un espacio a otro." } 
-            ],
+    "tags": [
+      {
+        "name": "Movimiento",
+        "info": "(rasgo) Una acción con este rasgo implica moverse de un espacio a otro."
+      }
+    ],
     "bullets": [
       "Puedes realizar múltiples Zancadas en tu turno, cada una gastando una acción."
     ]
@@ -15,16 +19,15 @@ data_movement = [
     "title": "Golpear [one-action]",
     "icon": "crossed-swords",
     "subtitle": " Ataque con un arma o  ataque desarmado.",
-    "description": "<p>Atacas con un arma que empuñas o con un ataque sin armas, de signando como objetivo a una criatura a tu alcance (para un ataque cuerpo a cuerpo) o dentro del rango de distancia (para un ataque a distancia). Haz una tirada de ataque utilizando el modificador al ataque para el arma o ataque sin armas que estás utilizando, y compara el resultado con la CA de la criatura objetivo para determinar el efecto. </p><p><br></p><ul><li><strong>Éxito crítico</strong> Haz una tirada de daño según el arma o el ataque sin armas, infligiendo doble daño (ver pág. 407 para las reglas sobre cómo se dobla el daño). </li><li><strong>Éxito </strong>Haz una tirada de daño según el arma o el ataque sin armas, infligiendo daño normal</li></ul>",
+    "description": "<p>Atacas con un arma que empuñas o con un ataque sin armas, de signando como objetivo a una criatura a tu alcance (para un ataque cuerpo a cuerpo) o dentro del rango de distancia (para un ataque a distancia). Haz una tirada de ataque utilizando el modificador al ataque para el arma o ataque sin armas que estás utilizando, y compara el resultado con la CA de la criatura objetivo para determinar el efecto. </p><p><br></p><ul><li><strong>Éxito crítico</strong> Haz una tirada de daño según el arma o el ataque sin armas, infligiendo doble daño (ver pág. 407 para las reglas sobre cómo se dobla el daño). </li><li><strong>Éxito </strong>Haz una tirada de daño según el arma o el ataque sin armas, infligiendo daño normal</li></ul><p><br></p><p><br></p><p><br></p><p><strong>Información extra</strong></p><p><br></p><p><strong>Rango de distancia y Alcance Manual de juego p.426</strong></p><p><br></p><p>Rango de distancia y Alcance Las aptitudes que generan un efecto suelen funcionar dentro de un rango de distancia o de un alcance específicos. La mayor parte de conjuros y de aptitudes indican un rango de distancia, que es la distancia máxima desde la criatura u objeto que crea el efecto en que se puede manifestar el mismo.</p><p> Las armas a distancia y las armas arrojadizas tienen un incremento de rango de distancia y los ataques con dichas armas funcionan normalmente hasta allí. Los ataques contra objetivos más allá de dicho rango de distancia sufren un penalizador -2, que empeora en -2 por cada múltiplo adicional, hasta un máximo de un penalizador -10 después de 5 incrementos de rango de distancia adicionales. Los ataques más allá de dicho rango de distancia no son posibles. Por ejemplo, utilizando un arco corto, tus ataques no sufren penalizador alguno contra un objetivo a un rango de distancia de hasta 60 pies (18 m), sufren un penalizador -2 si un objetivo está a más de 60 y menos de 120 pies (18 y 36 m) de distancia, un -4 si&nbsp;está a más de 120 y menos de 180 pies (36 y 54 m) de distancia, y así sucesivamente hasta un máximo de 360 pies (108 m).</p><p> El alcance es la distancia máxima a la que puedes llegar físicamente con tu cuerpo o con un arma. Los Golpes cuerpo a cuerpo se basan en el alcance. Tu alcance típicamente es de 5 pies (1,5 m) pero las armas con el rasgo alcance pueden extenderlo. Las criaturas más grandes pueden tener un alcance mayor; por ejemplo, un ogro tiene un alcance de 10 pies (3 m). A diferencia de cómo se mide la mayoría de distancias, un alcance de 10 pies (3 m) puede llegar a 2 casillas en diagonal. Los alcances mayores de 10 pies se miden normalmente así: un alcance de 20 pies (6 m) puede llegar a 3 casillas en diagonal, un alcance de 30 pies (9 m) puede llegar a 4 casillas y así sucesivamente.</p><p><br></p><p><strong>Penalizador por ataque múltiple&nbsp;Manual de juego p.402</strong></p><p><br></p><p>&nbsp;Cuantos más ataques llevas a cabo más allá del primero en un solo turno, menos preciso te vuelves, lo que se representa mediante el penalizador por ataque múltiple. La segunda vez que utilizas una acción de ataque durante tu turno, sufres un penalizador -5 a la prueba. La tercera vez que atacas, y en todos los ataques subsiguientes, sufres un penalizador -10 a la prueba. Cada prueba que tiene el rasgo ataque cuenta para tu penalizador por ataque múltiple, incluyendo Golpes, tira das de ataque de conjuros, ciertas acciones de habilidad como Empujar y muchas otras</p><p>. Algunas armas y aptitudes reducen los penalizadores por ataque múltiple, como por ejemplo las armas ágiles, que reducen dichos penalizadores a -4 en el segundo ataque o a -8 en ataques posteriores.</p><p>&nbsp;Has de calcular siempre tu penalizador por ataque múltiple basándote en el arma que utilizas en dicho ataque, no en las utiliza das en ataques anteriores. Por ejemplo, supongamos que estás empuñando una espada larga en una mano y una espada corta (que tiene el rasgo ágil) en la otra, y propinas tres Golpes con dichas armas a lo largo de tu turno. El primer Golpe de tu turno carecerá de penalizador, sea cual sea el arma que utilices. El segundo sufrirá un penalizador -5 si utilizas la espada larga o un penalizador -4 si utilizas la espada corta. El tercero sufrirá un penalizador -10 si utilizas la espada larga y un penalizador -8 si utilizas la espada corta, sean cuales sean las armas utilizadas en los Golpes previos.</p><p> El penalizador por ataque múltiple se aplica sólo durante tu turno, por lo que no tienes que llevar la cuenta de ello si puedes llevar a cabo un Golpe reactivo o una reacción similar que te permite dar un Golpe en el turno de otro.</p><p><br></p><p>&nbsp;<strong>Penalizador por rango de distancia Manual de juego p.402</strong></p><p><br></p><p> Las armas a distancia y las armas arrojadizas tienen indicado un <strong>incremento de rango de distancia</strong> y los ataques con ellas se vuelven menos precisos contra objetivos que están más lejos (el rango de distancia y sus incrementos se tratan en profundidad en la pág. 426). Si tu objetivo se encuentra dentro del incremento de rango de distancia indicado o menos, también llamado primer incremento de rango de distancia, no sufres penalizador alguno a la tirada de ataque. Si atacas más allá de dicho incremento de rango de distancia, sufres un penalizador -2 por cada incremento adicional más allá del primero. Puedes atacar con un arma a distancia o arrojadiza hasta a seis incrementos de rango de distancia, pero cuanto más lejos estés, más difícil te será acertar a tu objetivo.</p><p> Por ejemplo, el incremento de rango de distancia de una ballesta es 120 pies (36 m). Si estás disparando contra un objetivo que no está más lejos de dicha distancia, no sufres penalizador alguno debido al rango de distancia. Si está más allá de 120 pies (36 m) pero no más de 240 pies (72 m), sufres un penalizador -2 debido al rango de distancia. Si está más allá de 240 pies (72 m) pero no más de 360 pies (108 m), sufres un penalizador -4 debido al rango de distancia, y así sucesivamente hasta que lle gas al último incremento de rango de distancia: más allá de 600 pies (180 m) pero no más de 720 pies (216 m), donde sufres un penalizador -10 debido al rango de distancia.</p>",
     "reference": "Manual de juego p.419",
     "tags": [
-      { name: "Ataque", info: "<p>Cuando una criatura trata de dañar a otra, da un Golpe o utiliza otra acción de ataque. La mayor parte de ataques requiere una tirada de ataque y designa como objetivo la Clase de armadura. Modificador a la tirada de ataque cuerpo a cuerpo = modificador por Fue (opcionalmente modificador por Des para un arma sutil) + bonificador por competencia + otros bonificadores + penalizadores; modificador a la tirada de ataque a distancia = modificador por Des + bonificador por competencia + otros bonificadores + penalizadores. 10, 26, 275, 402 a 403 ataque de conjuros 403 ataque no letal 407 ataque sin armas 275, impactos críticos 401, 407 penalizador por ataque múltiple <strong>(-5 a tu segundo ataque, -10 a ataques posteriores)</strong> 402 a 403</p>" }
+      {
+        "name": "Ataque",
+        "info": "<p>Cuando una criatura trata de dañar a otra, da un Golpe o utiliza otra acción de ataque. La mayor parte de ataques requiere una tirada de ataque y designa como objetivo la Clase de armadura. Modificador a la tirada de ataque cuerpo a cuerpo = modificador por Fue (opcionalmente modificador por Des para un arma sutil) + bonificador por competencia + otros bonificadores + penalizadores; modificador a la tirada de ataque a distancia = modificador por Des + bonificador por competencia + otros bonificadores + penalizadores. 10, 26, 275, 402 a 403 ataque de conjuros 403 ataque no letal 407 ataque sin armas 275, impactos críticos 401, 407 penalizador por ataque múltiple <strong>(-5 a tu segundo ataque, -10 a ataques posteriores)</strong> 402 a 403</p>"
+      }
     ],
-    "bullets": [
-      "tu objetivo tiene que  estar a tu alcance (pág. 426)",
-      " si estás atacando con un arma a distancia, tu objetivo tiene que estar dentro de tu rango de distancia  (pág. 426). Las armas a distancia se vuelven menos efectivas cuan to más excedes sus incrementos de rango de distancia (pág. 403)",
-      "Golpear en múltiples ocasiones en un mismo turno tiene ganancias  menguantes. Se aplica un penalizador por ataque múltiple (pág.  402) a cada ataque después del primero."
-    ]
+    "bullets": []
   },
   {
     "title": "Lanzar un conjuro [two-actions] (Típicamente 2 acciones)",
@@ -37,7 +40,8 @@ data_movement = [
     ],
     "bullets": [
       "Debes cumplir con todos los requisitos indicados en la descripción del conjuro."
-    ]
+    ],
+    "tags": []
   },
   {
     "title": "Paso [one-action]",
@@ -45,7 +49,11 @@ data_movement = [
     "subtitle": "Moverse 5 pies sin desencadenar reacciones.",
     "description": "<p>&nbsp;<strong>Requisitos </strong>Tu Velocidad es por lo menos 10 pies (3 m). </p><p><br></p><p>Te mueves cuidadosamente 5 pies (1,5 m). A diferencia de la mayoría de tipos de movimiento, dar un Paso no desencadena reacciones (del tipo Golpe reactivo) que pueden ser desencadenadas por acciones de movimiento o por entrar o salir de una casilla. No puedes dar un Paso en terreno difícil (pág. 423) ni darlo utilizando una Velocidad diferente a tu Velocidad terrestre</p>",
     "reference": "Manual de juego p.417",
-    "tags": [ { name: "Movimiento", info: "(rasgo) Una acción con este rasgo implica moverse de un espacio a otro." } 
+    "tags": [
+      {
+        "name": "Movimiento",
+        "info": "(rasgo) Una acción con este rasgo implica moverse de un espacio a otro."
+      }
     ],
     "bullets": []
   },
@@ -56,6 +64,7 @@ data_movement = [
     "description": "<p>&nbsp;<strong>Requisitos </strong>Estás embrazando un escudo. </p><p><br></p><p>Colocas el escudo para que te proteja. Cuando has Alzado un escudo, obtienes el bonificador por circunstancia indicado a la CA. Tu escudo permanece alzado hasta el inicio de tu siguiente turno.</p>",
     "reference": "Manual de juego p.419",
     "traits": [],
-    "bullets": []
+    "bullets": [],
+    "tags": []
   }
 ];
