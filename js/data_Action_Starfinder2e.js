35b66bd5afb8c98bbeb020f49eaa8da2ab4ab571
Niuzio <antoniopadron511@gmail.com>
2026-03-09 01:22:06 -0400

    Update data_Action_Starfinder2e.js

--- js/data_Action_Starfinder2e.js
+++ js/data_Action_Starfinder2e.js
@@ -1,11 +1,11 @@
 data_Action_Starfinder2e = [
   {
-    title: "Proximamente",
-    icon: "star",
-    subtitle: "Proximamente",
-    description: "Proximamente",
-    reference: "Proximamente",
-    tags       : [ ],
-    bullets: [ ]
+    "title": "Arcos",
+    "icon": "crossed-swords",
+    "subtitle": "Arcos",
+    "description": "<p>si el objetivo del impacto crítico está adyacente a una superficie, se queda clavado a la misma por el proyectil. El objetivo queda inmovilizado y debe invertir una acción de Interactuar para hacer una prueba de Atletismo CD 10 y arrancar el proyectil; hasta que lo consiga no se podrá mover de su espacio. La criatura no se queda clavada si es incorporal, líquida o de alguna otra forma podría escapar sin esfuerzo (como por ejemplo siendo lo suficientemente grande como para que el proyectil no fuera un impedimento).</p>",
+    "reference": "Manual de jugador p.283",
+    "tags": [],
+    "bullets": []
   }
 ];
