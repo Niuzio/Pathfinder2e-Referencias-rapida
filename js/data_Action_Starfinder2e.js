18cc483e7582a23cebd03b44c694e7b1fd0218fc
Niuzio <antoniopadron511@gmail.com>
2026-03-09 01:34:14 -0400

    Update data_Action_Starfinder2e.js

--- js/data_Action_Starfinder2e.js
+++ js/data_Action_Starfinder2e.js
@@ -7,5 +7,140 @@ data_Action_Starfinder2e = [
     "reference": "Manual de jugador p.283",
     "tags": [],
     "bullets": []
+  },
+  {
+    "title": "Armas de asta",
+    "icon": "crossed-swords",
+    "subtitle": "Armas de asta",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>el objetivo es movido 5 pies (1,5 m) en una dirección elegida por ti. Éste es un movimiento forzado (pág. 422).</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Ballestas",
+    "icon": "crossed-swords",
+    "subtitle": "Ballestas",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>el objetivo sufre 1d8 daño persistente por sangrado. Obtienes un bonificador por objeto a este daño por sangrado igual al bonificador por objeto del arma a las tiradas de ataque.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Bombas",
+    "icon": "crossed-swords",
+    "subtitle": "Bombas",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>&nbsp;incrementa a 10 pies (3 m)el radio del daño por salpicadura de la bomba (si lo hay).</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Clavas",
+    "icon": "crossed-swords",
+    "subtitle": "Clavas",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>apartas de tí de un golpe al objetivo hasta 10 pies (3 m) en línea recta, eligiendo tú la dirección. Éste es un movi miento forzado (pág. 422).</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Cuchillos",
+    "icon": "crossed-swords",
+    "subtitle": "Cuchillos",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>&nbsp;el objetivo sufre 1d6 daño persistente por san grado. Obtienes un bonificador por objeto a este daño por sangrado igual al bonificador por objeto del arma a las tiradas de ataque.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Dardos",
+    "icon": "crossed-swords",
+    "subtitle": "Dardos",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>el objetivo sufre 1d6 daño persistente por sangra do. Obtienes un bonificador por objeto a este daño por san grado igual al bonificador por objeto del arma a las tiradas de ataque.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Escudos",
+    "icon": "crossed-swords",
+    "subtitle": "Escudos",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>&nbsp;apartas de ti de un empujón al objetivo 5 pies (1,5 m). Éste es un movimiento forzado (pág. 422).</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Espadas",
+    "icon": "crossed-swords",
+    "subtitle": "Espadas",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>&nbsp;el objetivo queda desequilibrado por tu ataque, quedando desprevenido hasta el inicio de tu siguiente turno.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Hachas",
+    "icon": "crossed-swords",
+    "subtitle": "Hachas",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>elige una criatura adyacente al objetivo inicial y dentro del alcance. Si su CA es menor que el resultado de tu tirada de ataque para el impacto crítico, inflige tanto daño a dicha criatura como el resultado del dado de daño de arma que tiraste (incluyendo los dados adicionales por su runa de golpe, si hubiera). Esta cantidad no se dobla, ni se aplican bonificadores ni otros dados adicionales a este daño.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Hondas",
+    "icon": "crossed-swords",
+    "subtitle": "Hondas",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>&nbsp;el objetivo ha de tener éxito en una salvación de Fortaleza contra tu CD de clase o quedar aturdido 1.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Lanzas",
+    "icon": "crossed-swords",
+    "subtitle": "Lanzas",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>las armas de este grupo atraviesan al objetivo, de bilitando sus ataques. Éste queda torpe 1 hasta el inicio de tu siguiente turno.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Manguales",
+    "icon": "crossed-swords",
+    "subtitle": "Manguales",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>el objetivo queda tumbado hasta que tiene éxi to en una salvación de Reflejos contra tu CD de clase.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Martillos",
+    "icon": "crossed-swords",
+    "subtitle": "Martillos",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>&nbsp;el objetivo queda tumbado hasta que tiene éxito en una salvación de Fortaleza contra tu CD de clase.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Pelea",
+    "icon": "crossed-swords",
+    "subtitle": "Pelea",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>&nbsp;el objetivo debe tener éxito en una salvación de For taleza contra tu CD de clase o quedar lentificado 1 hasta el f inal de tu siguiente turno.</p>",
+    "bullets": [],
+    "tags": []
+  },
+  {
+    "title": "Picos",
+    "icon": "crossed-swords",
+    "subtitle": "Picos",
+    "reference": "Manual de jugador p.283",
+    "description": "<p>El arma atraviesa cruelmente el objetivo, que sufre 2 daño adicional por dado de daño de arma.</p>",
+    "bullets": [],
+    "tags": []
   }
 ];
